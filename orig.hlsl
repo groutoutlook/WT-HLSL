87757596c186cb05eb95a54c4af0bfb560a6f229
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-06-25 17:10:38 +0700

    :sparkles: feat: transparent the neovim background color

--- orig.hlsl
+++ orig.hlsl
@@ -47,62 +47,22 @@ static const vec4 unit4 = vec4(1.0, 1.0, 1.0, 1.0);
 
 // --------------------
 
-#define LAYERS            5.0
-#define PI                3.141592654
-#define TAU               (2.0*PI)
-#define ROT(a)            mat2(cos(a), sin(a), -sin(a), cos(a))
+#define PI          3.141592654
+#define PI_2        (0.5*3.141592654)
+#define TAU         (2.0*PI)
+#define ROT(a)      mat2(cos(a), sin(a), -sin(a), cos(a))
 
-// License: Unknown, author: nmz (twitter: @stormoid), found: https://www.shadertoy.com/view/NdfyRM
-float sRGB(float t) { return mix(1.055*pow(t, 1./2.4) - 0.055, 12.92*t, step(t, 0.0031308)); }
-// License: Unknown, author: nmz (twitter: @stormoid), found: https://www.shadertoy.com/view/NdfyRM
-vec3 sRGB(in vec3 c) { return vec3 (sRGB(c.x), sRGB(c.y), sRGB(c.z)); }
+static const vec3  bcol        = vec3(1.0, 0.15, 0.25)*sqrt(0.5);
 
-// License: Unknown, author: Matt Taylor (https://github.com/64), found: https://64.github.io/tonemapping/
-vec3 aces_approx(vec3 v) {
-  v = max(v, 0.0);
-  v *= 0.6f;
-  float a = 2.51f;
-  float b = 0.03f;
-  float c = 2.43f;
-  float d = 0.59f;
-  float e = 0.14f;
-  return clamp((v*(a*v+b))/(v*(c*v+d)+e), 0.0f, 1.0f);
-}
+static const float logo_radius = 0.25;
+static const float logo_off    = 0.25;
+static const float logo_width  = 0.10;
 
-// License: Unknown, author: Unknown, found: don't remember
-float hash(float co) {
-  return fract(sin(co*12.9898) * 13758.5453);
-}
-
-// License: Unknown, author: Unknown, found: don't remember
-vec2 hash2(vec2 p) {
-  p = vec2(dot (p, vec2 (127.1, 311.7)), dot (p, vec2 (269.5, 183.3)));
-  return fract(sin(p)*43758.5453123);
-}
-
-// License: WTFPL, author: sam hocevar, found: https://stackoverflow.com/a/17897228/418488
-static const vec4 hsv2rgb_K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-vec3 hsv2rgb(vec3 c) {
-  vec3 p = abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www);
-  return c.z * mix(hsv2rgb_K.xxx, clamp(p - hsv2rgb_K.xxx, 0.0, 1.0), c.y);
-}
-#define HSV2RGB(c)  (c.z * mix(hsv2rgb_K.xxx, clamp(abs(fract(c.xxx + hsv2rgb_K.xyz) * 6.0 - hsv2rgb_K.www) - hsv2rgb_K.xxx, 0.0, 1.0), c.y))
-
-// License: Unknown, author: Unknown, found: don't remember
-float tanh_approx(float x) {
-//  return tanh(x);
-  float x2 = x*x;
-  return clamp(x*(27.0 + x2)/(27.0+9.0*x2), -1.0, 1.0);
-}
-
-float circle(vec2 p, float r) {
-  return length(p) - r;
-}
-
-// License: MIT, author: Inigo Quilez, found: https://iquilezles.org/articles/smin
-float pmin(float a, float b, float k) {
-    float h = clamp( 0.5+0.5*(b-a)/k, 0.0, 1.0 );
-    return mix( b, a, h ) - k*h*(1.0-h);
+// License: MIT, author: Pascal Gilcher, found: https://www.shadertoy.com/view/flSXRV
+float atan_approx(float y, float x) {
+  float cosatan2 = x / (abs(x) + abs(y));
+  float t = PI_2 - cosatan2 * PI_2;
+  return y < 0.0 ? -t : t;
 }
 
 // License: MIT OR CC-BY-NC-4.0, author: mercury, found: https://mercury.sexy/hg_sdf/
@@ -113,6 +73,115 @@ float mod1(inout float p, float size) {
   return c;
 }
 
+float spiralLength(float b, float a) {
+  // https://en.wikipedia.org/wiki/Archimedean_spiral
+  return 0.5*b*(a*sqrt(1.0+a*a)+log(a+sqrt(1.0+a*a)));
+}
+
+void spiralMod(inout vec2 p, float a) {
+  vec2 op     = p;
+  float b     = a/TAU;
+  float  rr   = length(op);
+  float  aa   = atan2(op.y, op.x);
+  rr         -= aa*b;
+  float nn    = mod1(rr, a);
+  float sa    = aa + TAU*nn;
+  float sl    = spiralLength(b, sa);
+  p           = vec2(sl, rr);
+}
+
+float dsegmentx(vec2 p, vec2 dim) {
+  p.x = abs(p.x);
+  float o = 0.5*max(dim.x-dim.y, 0.0);
+  if (p.x < o) {
+    return abs(p.y) - dim.y;
+  }
+  return length(p-vec2(o, 0.0))-dim.y;
+}
+
+vec3 digit(vec3 col, vec2 p, vec3 acol, vec3 icol, float aa, float n, float t) {
+  const int digits[16] = {
+    0x7D // 0
+  , 0x50 // 1
+  , 0x4F // 2
+  , 0x57 // 3
+  , 0x72 // 4
+  , 0x37 // 5
+  , 0x3F // 2
+  , 0x51 // 7
+  , 0x7F // 8
+  , 0x77 // 9
+  , 0x7B // A
+  , 0x3E // B
+  , 0x2D // C
+  , 0x5E // D
+  , 0x2F // E
+  , 0x2B // F
+  };
+  const vec2 dim = vec2(0.75, 0.075);
+  const float eps = 0.001;
+  vec2 ap = abs(p);
+  if (ap.x > (0.5+dim.y+eps)) return col;
+  if (ap.y > (1.0+dim.y+eps)) return col;
+  float m = mod(floor(n), 16.0);
+  int digit = digits[int(m)];
+
+  vec2 cp = (p-0.5);
+  vec2 cn = round(cp);
+
+  vec2 p0 = p;
+  p0.y -= 0.5;
+  p0.y = p0.y-0.5;
+  float n0 = round(p0.y);
+  p0.y -= n0;
+  float d0 = dsegmentx(p0, dim);
+
+  vec2 p1 = p;
+  vec2 n1 = sign(p1);
+  p1 = abs(p1);
+  p1 -= 0.5;
+  p1 = p1.yx;
+  float d1 = dsegmentx(p1, dim);
+
+  vec2 p2 = p;
+  p2.y = abs(p.y);
+  p2.y -= 0.5;
+  p2 = abs(p2);
+  float d2 = dot(normalize(vec2(1.0, -1.0)), p2);
+
+  float d = d0;
+  d = min(d, d1);
+
+  float sx = 0.5*(n1.x+1.0) + (n1.y+1.0);
+  float sy = -n0;
+  float s  = d2 > 0.0 ? (3.0+sx) : sy;
+#if defined(WINDOWS_TERMINAL)
+  // Can't get bit ops to work in kodelife
+  // Praying bit shift operations aren't TOO slow
+  vec3 scol = ((digit & (1 << int(s))) == 0) ? icol : acol;
+#else
+  vec3 scol = icol;
+#endif
+
+  col = mix(col, scol, smoothstep(aa, -aa, d)*t);
+  return col;
+}
+vec3 digit(vec3 col, vec2 p, vec3 acol, vec3 icol, float n, float t) {
+  vec2 aa2 = fwidth(p);
+  float aa = max(aa2.x, aa2.y);
+  return digit(col, p, acol, icol, aa, n, t);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+float hash(float co) {
+  return fract(sin(co*12.9898) * 13758.5453);
+}
+
+// License: Unknown, author: Unknown, found: don't remember
+float hash2(vec2 co) {
+  return fract(sin(dot(co.xy ,vec2(12.9898,58.233))) * 13758.5453);
+}
+
 // License: MIT OR CC-BY-NC-4.0, author: mercury, found: https://mercury.sexy/hg_sdf/
 vec2 mod2(inout vec2 p, vec2 size) {
   vec2 c = floor((p + size*0.5)/size);
@@ -120,248 +189,166 @@ vec2 mod2(inout vec2 p, vec2 size) {
   return c;
 }
 
-// License: MIT, author: Inigo Quilez, found: https://iquilezles.org/articles/intersectors
-float rayPlane(vec3 ro, vec3 rd, vec4 p) {
-  return -(dot(ro,p.xyz)+p.w)/dot(rd,p.xyz);
+// License: MIT, author: Inigo Quilez, found: https://iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
+float box(vec2 p, vec2 b) {
+  vec2 d = abs(p)-b;
+  return length(max(d,0.0)) + min(max(d.x,d.y),0.0);
 }
 
-vec3 toSpherical(vec3 p) {
-  float r   = length(p);
-  float t   = acos(p.z/r);
-  float ph  = atan2(p.y, p.x);
-  return vec3(r, t, ph);
+float circle(vec2 p, float r) {
+  return length(p) - r;
 }
 
-float sun(vec2 p) {
-  const float ch = 0.0125;
-  vec2 sp = p;
-  float d0 = circle(sp, 0.5);
-  float d = d0;
+float stripes(float d) {
+  const float cc = 0.42;
+  d = abs(d)-logo_width*cc;
+  d = abs(d)-logo_width*cc*0.5;
   return d;
 }
 
-float segmentx(vec2 p) {
-  float d0 = abs(p.y);
-  float d1 = length(p);
-  return p.x > 0.0 ? d0 : d1;
+vec4 merge(vec4 s0, vec4 s1) {
+  bool dt = s0.z < s1.z;
+  vec4 b = dt ? s0 : s1;
+  vec4 t = dt ? s1 : s0;
+
+  b.x *= 1.0 - exp(-max(80.0*(t.w), 0.0));
+
+  vec4 r = vec4(
+      mix(b.xy, t.xy, t.y)
+    , b.w < t.w ? b.z : t.z
+    , min(b.w, t.w)
+    );
+
+  return r;
 }
 
-float segmentx(vec2 p, float l) {
-  float hl = 0.5*l;
-  p.x = abs(p.x);
-  float d0 = abs(p.y);
-  float d1 = length(p-vec2(hl, 0.0));
-  return p.x > hl ? d1 : d0;
+vec4 figure_8(vec2 p, float aa) {
+  vec2  p1 = p-vec2(logo_off, -logo_off);
+  float d1 = abs(circle(p1, logo_radius));
+  float a1 = atan_approx(-p1.x, -p1.y);
+  float s1 = stripes(d1);
+  float o1 = d1 - logo_width;
+
+  vec2  p2 = p-vec2(logo_off, logo_off);
+  float d2 = abs(circle(p2, logo_radius));
+  float a2 = atan_approx(p2.x, p2.y);
+  float s2 = stripes(d2);
+  float o2 = d2 - logo_width;
+
+  vec4 c0 = vec4(smoothstep(aa, -aa, s1), smoothstep(aa, -aa, o1), a1, o1);
+  vec4 c1 = vec4(smoothstep(aa, -aa, s2), smoothstep(aa, -aa, o2), a2, o2);
+
+  return merge(c0, c1);
 }
 
-vec3 road(vec3 ro, vec3 rd, vec3 nrd, float glare, vec4 pl, out float pt) {
-  const float szoom   = 0.5;
-  const float bsz     = 25.0;
-  const float sm      = 1.0;
-  float off = abs(pl.w);
-  float t = rayPlane(ro, rd, pl);
-  pt = t;
+vec4 clogo(vec2 p, float aa, out float d) {
+  const mat2 rot0 = ROT(PI/4.0);
+  const mat2 rot1 = ROT(5.0*PI/4.0);
 
-  vec3 p  = ro+rd*t;
-  vec3 np = ro+nrd*t;
+//#define SINGLE8
 
-  vec2 pp   = p.xz;
-  vec2 npp  = np.xz;
-  vec2 opp  = pp;
+  float sgn = sign(p.y);
+#if !defined(SINGLE8)
+  p *= sgn;
+#endif
+  vec4 s0 = figure_8(p, aa);
+  vec4 s1 = figure_8(mul(rot0, p), aa);
+  vec4 s2 = figure_8(p-vec2(-0.5, 0.0), aa);
+  vec4 s3 = figure_8(mul(rot1, p), aa);
 
-  float aa  = length(npp-pp)*sqrt(0.5);
-  pp.y += -20.0*TIME;
+  // This is very hackish to get it to look reasonable
 
-  vec3 gcol = unit3*(0.0);
+  const float off = -PI;
+  s1.z -= off;
+  s3.z -= off;
 
-  float dr = abs(pp.x)-off;
-  vec2 cp = pp;
-  mod1(cp.y, 6.0*off);
-  vec2 sp = pp;
-  sp.x = abs(sp.x);
-  mod1(sp.y, off);
-  float dcl = segmentx(cp.yx, 1.5*off);
-  float dsl = segmentx((sp-vec2(0.95*off, 0.0)).yx, off*0.5);
+  vec4 s = s0;
+#if !defined(SINGLE8)
+  s = merge(s, s1);
+  s = merge(s, s2);
+  s = merge(s, s3);
+#endif
 
-  vec2 mp = pp;
-  mod2(mp, unit2*(off*0.5));
-
-  vec2 dp = abs(mp);
-  float d = dp.x;
-  d = pmin(d, dp.y, sm);
-  d = max(d, -dr);
-  d = min(d, dcl);
-  d = min(d, dsl);
-  vec2 s2 = sin(TIME+2.0*p.xz/off);
-  float m = mix(0.75, 0.9, tanh_approx(s2.x+s2.y));
-  m *= m;
-  m *= m;
-  m *= m;
-  vec3 hsv = vec3(0.4+mix(0.5, 0.0, m), tanh_approx(0.15*mix(30.0, 10.0, m)*d), 1.0);
-  float fo = exp(-0.04*max(abs(t)-off*2., 0.0));
-  vec3 bcol = hsv2rgb(hsv);
-  gcol += 2.0*bcol*exp(-0.1*mix(30.0, 10.0, m)*d)*fo;
-
-  float sh;
-  float sdb;
-  sh = tanh_approx(sh);
-  sdb *= 0.075;
-  sdb *= sdb;
-  sdb += 0.05;
-
-
-  gcol = t > 0.0 ? gcol : unit3*(0.0);
-  return gcol;
+  d = s.w;
+  return vec4(mix(0.025*bcol, bcol, s.x), s.y);
 }
 
-vec3 stars(vec2 sp, float hh) {
-  vec3 col = unit3*(0.0);
+vec3 logoEffect(vec3 col, vec2 p, vec2 pp, float aa) {
+  float d;
+  vec4 ccol = clogo(p, aa, d);
 
-  const float m = LAYERS;
-  hh = tanh_approx(20.0*hh);
+  const float period = TAU*10.0;
+  float ss = sin(period*d-TIME*TAU/10.0);
+  const float off = 0.2;
+  float doff = period*aa*cos(off);
+//  col = mix(col, col*0.125, smoothstep(doff, -doff, abs(ss)-off));
+  col = mix(col, ccol.xyz, ccol.w);
+  return col;
+}
 
-  for (float i = 0.0; i < m; ++i) {
-    vec2 pp = sp+0.5*i;
-    float s = i/(m-1.0);
-    vec2 dim  = unit2*(mix(0.05, 0.003, s)*PI);
-    vec2 np = mod2(pp, dim);
-    vec2 h = hash2(np+127.0+i);
-    vec2 o = -1.0+2.0*h;
-    float y = sin(sp.x);
-    pp += o*dim*0.5;
-    pp.y *= y;
-    float l = length(pp);
+vec3 spiralEffect(vec3 col, vec2 p, vec2 pp, float aa) {
+  vec2 sp = p;
+  spiralMod(sp, .5);
 
-    float h1 = fract(h.x*1667.0);
-    float h2 = fract(h.x*1887.0);
-    float h3 = fract(h.x*2997.0);
+  vec2 dp = sp;
+  float dz = 0.0125;
+  dp /= dz;
+  aa /= dz;
+  float dny = mod1(dp.y, 3.06);
+  float dhy = hash(dny+1234.5);
+  dp.x = -dp.x;
+  float ltm = (TIME+1234.5)*mix(2.0, 10.0, (dhy))*0.125;
+  dp.x -= ltm;
+  float opx = dp.x;
+  float dnx = mod1(dp.x, 1.5);
+  const float stepfx = 0.125*0.25;
+  float fx  = -2.0*stepfx*ltm+stepfx*dnx;
+  float fnx = floor(fx);
+  float ffx = fract(fx);
+  float dht = hash(fnx);
+  float dhx = hash(dnx);
+  float dh  = fract(dht+dhx+dhy);
 
-    float ll = mix(0.5, 1.0, h1);
-    vec3 scol = mix(8.0*h2, 0.25*h2*h2, s)*HSV2RGB(vec3(0.8, 0.8, 2.0))*ll;
+  float l = length(p);
+  float t = smoothstep(0.4, 0.5, l);
 
-    vec3 ccol = col + exp(-(mix(6000.0, 2000.0, hh)/mix(2.0, 0.25, s))*max(l-0.001, 0.0))*scol;
-    ccol *= mix(0.125, 1.0, smoothstep(1.0, 0.99, sin(0.25*TIME+TAU*h.y)));
-    col = h3 < y ? ccol : col;
-  }
+  const vec3 hcol = clamp(1.5*sqrt(bcol)+unit3*(0.2), 0.0, 1.0);
+  const vec3 acol = bcol;
+  const vec3 icol = acol*0.1;
+
+  float fo = (smoothstep(0.0, 1.0, ffx));
+  float ff = smoothstep(1.0-2.0*sqrt(stepfx), 1.0, ffx*ffx);
+  col = digit(col, dp, mix(acol, hcol, ff), icol, aa, 100.0*dh, fo*t);
+
+#if defined(CURSOR)
+  float fc = smoothstep(1.0-stepfx, 1.0, ffx);
+  const float rb = 0.2;
+  float db = box(dp, vec2(0.5, 1.0))-rb;
+
+  col = mix(col, mix(col, hcol, 0.33*fc*fc), smoothstep(aa, -aa, db)*t);
+#endif
 
   return col;
 }
 
-vec3 meteorite(vec2 sp) {
-  const float period = 3.0;
-  float mtime = mod(TIME, period);
-  float ntime = floor(TIME/period);
-  float h0 = hash(ntime+123.4);
-  float h1 = fract(1667.0*h0);
-  float h2 = fract(9967.0*h0);
-  vec2 mp = sp;
-  mp.x += -1.0;
-  mp.y += -0.5*h1;
-  mp.y += PI*0.5;
-  mp = mul(ROT(PI+mix(-PI/4.0, PI/4.0, h0)), mp);
-  float m = mtime/period;
-  mp.x += mix(-1.0, 2.0, m);
-
-  float d0 = length(mp);
-  float d1 = segmentx(mp);
-
-  vec3 col = unit3*(0.0);
-
-  col += 0.5*exp(-4.0*max(d0, 0.0))*exp(-1000.0*max(d1, 0.0));
-  col *= 2.0*HSV2RGB(vec3(0.8, 0.5, 1.0));
-  float fl = smoothstep(-0.5, 0.5, sin(12.0*TAU*TIME));
-  col += mix(1.0, 0.5, fl)*exp(-mix(100.0, 150.0, fl)*max(d0, 0.0));
-
-  col = h2 > 0.8 ? col: unit3*(0.0);
+vec3 glowEffect(vec3 col, vec2 p, vec2 pp, float aa) {
+  float d = length(p);
+  col += 0.25*bcol*exp(-9.0*max(d-2.0/3.0, 0.0));
   return col;
 }
 
-vec3 skyGrid(vec2 sp) {
-  const float m = 1.0;
-
-  const vec2 dim = unit2*(1.0/12.0*PI);
-  float y = sin(sp.x);
-  vec2 pp = sp;
-  vec2 np = mod2(pp, dim*vec2(1.0/floor(1.0/y), 1.0));
-
-  vec3 col = unit3*(0.0);
-
-  float d = min(abs(pp.x), abs(pp.y*y));
-
+vec3 effect(vec2 p, vec2 pp) {
   float aa = 2.0/RESOLUTION.y;
-
-  col += 0.25*vec3(0.5, 0.5, 1.0)*exp(-2000.0*max(d-0.00025, 0.0));
-
+  vec3 col  = unit3*(0.0);
+  col = spiralEffect(col, p, pp, aa);
+  col = glowEffect(col,p, pp, aa);
+  col = logoEffect(col, mul(ROT(-0.05*TIME), p), pp, aa);
+  col *= smoothstep(1.25, 0.5, length(pp));
+  col = sqrt(col);
   return col;
 }
 
-
-vec3 sunset(vec2 sp, vec2 nsp) {
-  const float szoom   = 0.5;
-  float aa = length(nsp-sp)*sqrt(0.5);
-  sp -= vec2(vec2(0.5, -0.5)*PI);
-  sp /= szoom;
-  sp = sp.yx;
-  sp.y += 0.22;
-  sp.y = -sp.y;
-  float ds = sun(sp)*szoom;
-
-  vec3 bscol = hsv2rgb(vec3(fract(0.7-0.25*(sp.y)), 1.0, 1.0));
-  vec3 gscol = 0.75*sqrt(bscol)*exp(-50.0*max(ds, 0.0));
-  vec3 scol = mix(gscol, bscol, smoothstep(aa, -aa, ds));
-  return scol;
-}
-
-vec3 glow(vec3 ro, vec3 rd, vec2 sp, vec3 lp) {
-  float ld = max(dot(normalize(lp-ro), rd),0.0);
-  float y = -0.5+sp.x/PI;
-  y = max(abs(y)-0.02, 0.0)+0.1*smoothstep(0.5, PI, abs(sp.y));
-  float ci = pow(ld, 10.0)*2.0*exp(-25.0*y);
-  float h = 0.65;
-  vec3 col = hsv2rgb(vec3(h, 0.75, 0.35*exp(-15.0*y)))+HSV2RGB(vec3(0.8, 0.75, 0.5))*ci;
-  return col;
-}
-
-vec3 neonSky(vec3 ro, vec3 rd, vec3 nrd, out float gl) {
-  const vec3 lp       = 500.0*vec3(0.0, 0.25, -1.0);
-  const vec3 skyCol   = HSV2RGB(vec3(0.8, 0.75, 0.05));
-
-
-  float glare = pow(abs(dot(rd, normalize(lp))), 20.0);
-
-  vec2 sp   = toSpherical(rd.xzy).yz;
-  vec2 nsp  = toSpherical(nrd.xzy).yz;
-  vec3 grd  = rd;
-  grd.xy = mul(ROT(0.025*TIME), grd.xy);
-  vec2 spp = toSpherical(grd).yz;
-
-  float gm = 1.0/abs(rd.y)*mix(0.005, 2.0, glare);
-  vec3 col = skyCol*gm;
-  float ig = 1.0-glare;
-  col += glow(ro, rd, sp, lp);
-  if (rd.y > 0.0) {
-    col += sunset(sp, nsp);
-    col += stars(sp, 0.0)*ig;
-    col += skyGrid(spp)*ig;
-    col += meteorite(sp)*ig;
-  }
-  gl = glare;
-  return col;
-}
-
-vec3 color(vec3 ro, vec3 rd, vec3 nrd) {
-  const float off1  = -20.0;
-  const vec4 pl1    = vec4(normalize(vec3(0.0, 1.0, 0.15)), -off1);
-  float glare;
-  vec3 col = neonSky(ro, rd, nrd, glare);
-  if (rd.y < 0.0) {
-    float t;
-    col += road(ro, rd, nrd, glare, pl1, t);
-  }
-  return col;
-}
-
-
+static const float3 chromaKey = float3(26.0f / 0xFF, 29.0f / 0xFF, 35.0f / 0xFF);
 //
 // PS_OUTPUT ps_main(in PS_INPUT In)
 #if defined(WINDOWS_TERMINAL)
@@ -372,35 +359,26 @@ float4 ps_main(float4 pos : SV_POSITION, float2 tex : TEXCOORD) : SV_TARGET
 {
   vec2 q = tex;
   vec2 p = -1.0 + 2.0*q;
+  vec2 pp = p;
 #if defined(WINDOWS_TERMINAL)
   p.y = -p.y;
 #endif
-  p.x *= RESOLUTION.x/RESOLUTION.y;
+  float r = RESOLUTION.x/RESOLUTION.y;
+  p.x *= r;
 
-  float aa = 2.0/RESOLUTION.y;
-
-  vec3 ro = vec3(0.0, 0.0, 10.0);
-  vec3 la = vec3(0.0, 2.0, 0.0);
-  vec3 up = vec3(0.0, 1.0, 0.0);
-
-  vec3 ww = normalize(la - ro);
-  vec3 uu = normalize(cross(up, ww ));
-  vec3 vv = (cross(ww,uu));
-  const float fov = tan(TAU/6.0);
-  vec2 np = p + unit2*(aa);
-  vec3 rd = normalize(-p.x*uu + p.y*vv + fov*ww);
-  vec3 nrd = normalize(-np.x*uu + np.y*vv + fov*ww);
-
-
-  vec3 col = unit3*(0.0);
-  col = color(ro, rd, nrd);
-  col = aces_approx(col);
-  col = sRGB(col);
+  vec3 col = effect(p, pp);
 
   vec4 fg = shaderTexture.Sample(samplerState, q);
   vec4 sh = shaderTexture.Sample(samplerState, q-2.0*unit2/RESOLUTION.xy);
+
   col = mix(col, 0.0*unit3, sh.w);
   col = mix(col, fg.xyz, fg.w);
 
-  return vec4(col, 1.0);
+  if(col.r == chromaKey.r && col.g == chromaKey.g && col.b == chromaKey.b)
+  {
+    return float4(0.0f, 0.0f, 0.0f, 0.0f);
+  }
+  else{
+    return vec4(col, 1.0);
+  }
 }
